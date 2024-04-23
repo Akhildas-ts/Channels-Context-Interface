3f4c8d8e2079b73d958cf7e697804585ac88f4c6
Akhil Das <akhilpic1@gmail.com>
2024-04-23 06:00:48 +0530

    contex cannellation with http request

--- context/context.go
+++ context/context.go
@@ -1,38 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func main() {
 
+
+     // if the http method is over these time , the request will cancel
+	timeOutContext,cancel := context.WithTimeout(context.Background(), time.Millisecond *500)
+
+	defer cancel()
 	// create http
 
-	req, err := http.NewRequest(http.MethodGet, "https://www.istockphoto.com/photos/kochi-india ", nil)
+	req, err := http.NewRequestWithContext(timeOutContext,http.MethodGet, "https://www.istockphoto.com/photos/kochi-india ", nil)
 
 	if err != nil {
 		panic(err)
 	}
 
 	//perform http request
-	res,err := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
 
-
-	if err!= nil{
+	if err != nil {
 		panic(err)
 	}
 
 	defer res.Body.Close()
 
+	//get image from response
 
-
-	//get image from response 
-
-	imageData,err := ioutil.ReadAll(res.Body)
+	imageData, err := ioutil.ReadAll(res.Body)
 
 	fmt.Println("downolad image of size %d/n", len(imageData))
-
 
 }
